fa524a223a9667580f1fa58da3d4aadfa257ea3d
YFshadaow <renzd2001@gmail.com>
2025-02-28 20:30:15 +0800

    Add Thorns, Trigger, Uncrowned and Warrior.

---
+++ src/main/java/fun/kaituo/kitbattle/kits/Warrior.java
@@ -0,0 +1,11 @@
+package fun.kaituo.kitbattle.kits;
+
+import fun.kaituo.kitbattle.util.PlayerData;
+import org.bukkit.entity.Player;
+
+@SuppressWarnings("unused")
+public class Warrior extends PlayerData {
+    public Warrior(Player p) {
+        super(p);
+    }
+}
