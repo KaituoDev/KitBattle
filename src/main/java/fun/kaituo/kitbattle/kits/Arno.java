2afe76c47a27e2cba7e101ce3cf79701d1e2a0d9
mn-yinduan <danny0804@126.com>
2025-03-01 14:38:00 +0800

    feat:add kits Arno and Poseidon

---
+++ src/main/java/fun/kaituo/kitbattle/kits/Arno.java
@@ -0,0 +1,10 @@
+package fun.kaituo.kitbattle.kits;
+
+import fun.kaituo.kitbattle.util.PlayerData;
+import org.bukkit.entity.Player;
+
+public class Arno extends PlayerData {
+    public Arno(Player p){
+        super(p);
+    }
+}
