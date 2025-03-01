8439dfec87293be895479ec6f1830dfc4418ab5c
mn-yinduan <danny0804@126.com>
2025-03-01 16:24:34 +0800

    feat:add kit Arno and Poseidon

--- src/main/java/fun/kaituo/kitbattle/kits/Poseidon.java
+++ src/main/java/fun/kaituo/kitbattle/kits/Poseidon.java
@@ -1,10 +1,12 @@
+
 package fun.kaituo.kitbattle.kits;
 
 import fun.kaituo.kitbattle.util.PlayerData;
 import org.bukkit.entity.Player;
 
 public class Poseidon extends PlayerData {
-    public Poseidon(Player p){
+    public Poseidon(Player p) {
         super(p);
     }
 }
+
